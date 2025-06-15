0589686b72058886a5967c9ff48374dd83d5b504
Louis FRADIN <louis.fradin@gmail.com>
2025-06-15 18:38:32 +0200

    fix: add ci

---
+++ .dagger/main.go
@@ -0,0 +1,44 @@
+// A generated module for GoClient functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+
+	"github.com/cryptellation/go-client/dagger/internal/dagger"
+)
+
+type PythonClient struct{}
+
+// Publish a new release
+func (ci *PythonClient) PublishTag(
+	ctx context.Context,
+	sourceDir *dagger.Directory,
+	user *string,
+	token *dagger.Secret,
+) error {
+	// Create Git repo access
+	repo, err := NewGit(ctx, NewGitOptions{
+		SrcDir: sourceDir,
+		User:   user,
+		Token:  token,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Publish new tag
+	return repo.PublishTagFromReleaseTitle(ctx)
+}
